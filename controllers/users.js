ddaecb67dbcc6439e3ef613eeec6f9e106e8b69c
Simo Siren <simo.siren@stud.pointcollege.fi>
2020-12-14 22:38:20 +0200

    fixed naming bug on get User

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 // SELF-REGISTER ENDPOINT
 
@@ -56,10 +57,11 @@ usersRouter.get('/', async (request, response, next) => {
     }
 
     try {
-        const users = await Users.find({})
+        const users = await User.find({})
         response.json(users.map(u => u.toJSON()))
     }
     catch (exception) {
+        console.log(users)
         next(exception)
     }
 })
